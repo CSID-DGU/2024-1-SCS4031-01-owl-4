b63b2b230dd003cc99909225cd5715628056eb87
bbbang105 <2018111366@dgu.ac.kr>
2024-04-10 22:54:08 +0900

    [chore] : 디렉토리명 변경

--- backend/src/main/java/org/dgu/backend/BackendApplication.java
+++ backend/src/main/java/org/dgu/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package org.example.backend;
+package org.dgu.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/org/dgu/backend/BackendApplicationTests.java
+++ backend/src/test/java/org/dgu/backend/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package org.example.backend;
+package org.dgu.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
