6bc94ca5cc3b9202141a9ce39e71ddf7c405f603
bbbang105 <2018111366@dgu.ac.kr>
2024-04-12 02:15:08 +0900

    #3 [feat] : 한글명을 기준으로 Market 객체를 반환하는 기능 구현

--- backend/src/main/java/org/dgu/backend/repository/MarketRepository.java
+++ backend/src/main/java/org/dgu/backend/repository/MarketRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MarketRepository extends JpaRepository<Market,Long> {
-
+    Market findByKoreanName(String marketKoreanName);
 }
