28665c46ac9cd32fec8a2105d2b47ed6fcc333fd
bbbang105 <2018111366@dgu.ac.kr>
2024-04-12 04:02:18 +0900

    #3 [feat] : 캔들 종류 엔터티 추가

---
+++ backend/src/main/java/org/dgu/backend/domain/Candle.java
@@ -0,0 +1,28 @@
+package org.dgu.backend.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Data
+@Table(name = "candles")
+public class Candle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "candles_id")
+    private Long id;
+
+    @Column(name = "candles_name")
+    private String name;
+
+    @Column(name = "korean_name")
+    private String koreanName;
+
+    @OneToMany(mappedBy = "candle", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CandleInfo> candleInfos;
+}
