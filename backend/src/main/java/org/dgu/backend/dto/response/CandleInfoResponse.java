be45ced2e3a1e40f8e6d66d4e49fa41ac408214f
bbbang105 <2018111366@dgu.ac.kr>
2024-04-11 20:34:35 +0900

    #3 [feat] : 캔들 DTO 추가

---
+++ backend/src/main/java/org/dgu/backend/dto/response/CandleInfoResponse.java
@@ -0,0 +1,43 @@
+package org.dgu.backend.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class CandleInfoResponse {
+    @JsonProperty("market")
+    private String marketName;
+
+    @JsonProperty("candle_date_time_kst")
+    private String dateTime;
+
+    @JsonProperty("opening_price")
+    private Double openingPrice;
+
+    @JsonProperty("high_price")
+    private Double highPrice;
+
+    @JsonProperty("low_price")
+    private Double lowPrice;
+
+    @JsonProperty("trade_price")
+    private Double tradePrice;
+
+    @JsonProperty("timestamp")
+    private Long timestamp;
+
+    @JsonProperty("candle_acc_trade_price")
+    private Double accTradePrice;
+
+    @JsonProperty("candle_acc_trade_volume")
+    private Double accTradeVolume;
+
+    @JsonProperty("prev_closing_price")
+    private Double prevClosingPrice;
+
+    @JsonProperty("change_price")
+    private Double changePrice;
+
+    @JsonProperty("change_rate")
+    private Double changeRate;
+}
