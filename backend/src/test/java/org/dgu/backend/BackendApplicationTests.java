03044e31203f463c0d735c3a194a3b40a6ce9801
bbbang105 <2018111366@dgu.ac.kr>
2024-04-14 02:26:58 +0900

    #3 [fix] : 빌드 시 테스트 오류 해결

--- backend/src/test/java/org/dgu/backend/BackendApplicationTests.java
+++ backend/src/test/java/org/dgu/backend/BackendApplicationTests.java
@@ -3,7 +3,7 @@ package org.dgu.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BackendApplicationTests.class)
 class BackendApplicationTests {
 
 	@Test
