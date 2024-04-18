3ba23d69d4d10b7f1d512401c4d8d51b7790c726
bbbang105 <2018111366@dgu.ac.kr>
2024-04-18 16:44:37 +0900

    #5 [refactor] : 초기 삽입 SQL문 수정

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,24 +1,15 @@
-CREATE TABLE IF NOT EXISTS candles (
-                                       candles_id INT NOT NULL AUTO_INCREMENT,
-                                       candles_name VARCHAR(255),
-    korean_name VARCHAR(255),
-    PRIMARY KEY (candles_id)
-    );
+INSERT INTO candles (candles_name, korean_name) VALUES
 
-INSERT INTO candles (candles_id, candles_name, korean_name)
-SELECT * FROM (
-                  SELECT 1, 'minutes1', '1분봉' UNION ALL
-                  SELECT 2, 'minutes3', '3분봉' UNION ALL
-                  SELECT 3, 'minutes5', '5분봉' UNION ALL
-                  SELECT 4, 'minutes10', '10분봉' UNION ALL
-                  SELECT 5, 'minutes15', '15분봉' UNION ALL
-                  SELECT 6, 'minutes30', '30분봉' UNION ALL
-                  SELECT 7, 'minutes60', '60분봉' UNION ALL
-                  SELECT 8, 'minutes240', '240분봉' UNION ALL
-                  SELECT 9, 'days', '일봉' UNION ALL
-                  SELECT 10, 'weeks', '주봉' UNION ALL
-                  SELECT 11, 'months', '월봉'
-              ) AS tmp
-WHERE NOT EXISTS (
-    SELECT candles_id FROM candles WHERE candles_id IN (1)
-);
+                  ('minutes1', '1분봉'),
+                  ('minutes3', '3분봉'),
+                  ('minutes5', '5분봉'),
+                  ('minutes10', '10분봉'),
+                  ('minutes15', '15분봉'),
+                  ('minutes30', '30분봉'),
+                  ('minutes60', '60분봉'),
+                  ('minutes240', '240분봉'),
+                  ('days', '일봉'),
+                  ('weeks', '주봉'),
+                  ('months', '월봉')
+
+ON DUPLICATE KEY UPDATE candles_id = candles_id;
