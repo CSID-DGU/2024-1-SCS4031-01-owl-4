156934b558b89265b9cc865c3b76db6717910a6a
bbbang105 <2018111366@dgu.ac.kr>
2024-04-18 17:16:40 +0900

    #5 [feat] : 응답 객체 통일 & ResponseEntity 묶어 반환하는 기능 구현

---
+++ backend/src/main/java/org/dgu/backend/common/ApiResponse.java
@@ -0,0 +1,30 @@
+package org.dgu.backend.common;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.dgu.backend.common.code.BaseCode;
+import org.dgu.backend.common.code.BaseErrorCode;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+@RequiredArgsConstructor
+public class ApiResponse<T> {
+    @JsonProperty("isSuccess")
+    private final Boolean isSuccess;
+    private final String code;
+    private final String message;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private final T payload;
+    public static <T> ResponseEntity<ApiResponse<T>> onSuccess(BaseCode code, T data) {
+        ApiResponse<T> response = new ApiResponse<>(true, code.getReasonHttpStatus().getCode(), code.getReasonHttpStatus().getMessage(), data);
+        return ResponseEntity.status(code.getReasonHttpStatus().getHttpStatus()).body(response);
+    }
+
+    public static <T> ResponseEntity<ApiResponse<T>> onFailure(BaseErrorCode code) {
+        ApiResponse<T> response = new ApiResponse<>(false, code.getReasonHttpStatus().getCode(), code.getReasonHttpStatus().getMessage(), null);
+        return ResponseEntity.status(code.getReasonHttpStatus().getHttpStatus()).body(response);
+    }
+}
