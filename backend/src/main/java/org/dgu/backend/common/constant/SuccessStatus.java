aac2c800ee4562a578cee618d55f706e144fbdf3
bbbang105 <2018111366@dgu.ac.kr>
2024-04-18 17:17:54 +0900

    #5 [feat] : 성공 시 응답 Enum 추가

---
+++ backend/src/main/java/org/dgu/backend/common/constant/SuccessStatus.java
@@ -0,0 +1,41 @@
+package org.dgu.backend.common.constant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.dgu.backend.common.code.BaseCode;
+import org.dgu.backend.common.dto.ReasonDto;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum SuccessStatus implements BaseCode {
+    // 전역 응답 코드
+    _OK(HttpStatus.OK, "200", "성공입니다."),
+    _CREATED(HttpStatus.CREATED, "201", "생성에 성공했습니다."),
+
+    // 커스텀 응답 코드
+    _CREATED_ACCESS_TOKEN(HttpStatus.CREATED, "201", "액세스 토큰 재발행에 성공했습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    @Override
+    public ReasonDto getReason() {
+        return ReasonDto.builder()
+                .isSuccess(true)
+                .code(code)
+                .message(message)
+                .build();
+    }
+
+    @Override
+    public ReasonDto getReasonHttpStatus() {
+        return ReasonDto.builder()
+                .isSuccess(true)
+                .httpStatus(httpStatus)
+                .code(code)
+                .message(message)
+                .build();
+    }
+}
