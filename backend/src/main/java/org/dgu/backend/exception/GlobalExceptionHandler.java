ff46a6971672c480580ccd9b448dc63aca1a0215
bbbang105 <2018111366@dgu.ac.kr>
2024-04-21 00:01:57 +0900

    #7 [feat] : 토큰 에러 처리 핸들러 구현

---
+++ backend/src/main/java/org/dgu/backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package org.dgu.backend.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.dgu.backend.common.ApiResponse;
+import org.dgu.backend.common.code.BaseErrorCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(TokenException.class)
+    public ResponseEntity<ApiResponse<BaseErrorCode>> handleTokenException(TokenException e) {
+        TokenErrorResult errorResult = e.getTokenErrorResult();
+        return ApiResponse.onFailure(errorResult);
+    }
+}
