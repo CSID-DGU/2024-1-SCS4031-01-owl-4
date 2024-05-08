17135a16023ffcd049ce56bf1fb1484ec8c95c98
bbbang105 <2018111366@dgu.ac.kr>
2024-05-08 14:03:26 +0900

    #13 [refactor] : 성공 시 응답 코드 보내도록 변경

--- backend/src/main/java/org/dgu/backend/controller/CandleInfoController.java
+++ backend/src/main/java/org/dgu/backend/controller/CandleInfoController.java
@@ -1,9 +1,12 @@
 package org.dgu.backend.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.dgu.backend.common.ApiResponse;
+import org.dgu.backend.common.constant.SuccessStatus;
 import org.dgu.backend.service.CandleInfoService;
 import org.dgu.backend.util.CandleDataCollector;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -15,7 +18,7 @@ public class CandleInfoController {
     private final CandleInfoService candleInfoService;
     private final CandleDataCollector candleDataCollector;
 
-    // 업비트에서 데이터를 가져오는 API
+    // 업비트에서 캔들차트를 가져오는 API
     @GetMapping("/candle/info")
     public void getCandleInfo(
             @RequestParam("market") String koreanName,
@@ -28,11 +31,13 @@ public class CandleInfoController {
 
     // 원하는 가상화폐 & 기간 & 캔들 종류에 따른 데이터를 가져오는 API
     @GetMapping("/candle/info/all")
-    public void collectBitcoinCandleData(
+    public ResponseEntity<ApiResponse<Object>> collectBitcoinCandleData(
             @RequestParam("market") String koreanName,
             @RequestParam(value = "to", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime to,
             @RequestParam("candle_type") String candleType) {
 
         candleDataCollector.collectData(koreanName, to, candleType);
+
+        return ApiResponse.onSuccess(SuccessStatus.SUCCESS_CANDLE_INFOS);
     }
 }
