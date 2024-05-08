916768cfed077a511bd2d0d64755fd3d9c995d56
bbbang105 <2018111366@dgu.ac.kr>
2024-05-08 13:57:03 +0900

    #13 [feat] : 포트폴리오 엔터티 & 레포지토리 추가

---
+++ backend/src/main/java/org/dgu/backend/domain/Portfolio.java
@@ -0,0 +1,70 @@
+package org.dgu.backend.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "portfolios")
+public class Portfolio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "portfolios_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "users_id", foreignKey = @ForeignKey(name = "portfolios_fk_users_id"))
+    private User user;
+
+    @Column(name = "portfolios_uuid", columnDefinition = "BINARY(16)", unique = true)
+    private UUID portfolioId;
+
+    @Column(name = "title", nullable = false, length = 20)
+    private String title;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "trading_start_date", length = 20)
+    private LocalDateTime tradingStartDate;
+
+    @Column(name = "trading_end_date", length = 20)
+    private LocalDateTime tradingEndDate;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, length = 20)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", length = 20)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "saved_at", length = 20)
+    private LocalDateTime savedAt;
+
+    @Column(name = "is_saved", nullable = false, length = 1)
+    private int isSaved;
+
+    @OneToOne(mappedBy = "portfolio", cascade = CascadeType.ALL, orphanRemoval = true)
+    private PortfolioOption portfolioOption;
+
+    @OneToOne(mappedBy = "portfolio", cascade = CascadeType.ALL, orphanRemoval = true)
+    private PortfolioResult portfolioResult;
+
+    @Builder
+    public Portfolio(User user, UUID portfolioId, String title, String description) {
+        this.user = user;
+        this.portfolioId = portfolioId;
+        this.title = title;
+        this.description = description;
+    }
+}
