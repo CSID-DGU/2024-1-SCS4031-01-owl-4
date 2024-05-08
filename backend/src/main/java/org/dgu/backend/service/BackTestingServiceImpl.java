fdf7e4a0253082dcc4e5d6a1a3d91d12cb6bc562
bbbang105 <2018111366@dgu.ac.kr>
2024-05-08 14:04:51 +0900

    #13 [feat] : 설정한 기간의 캔들 차트를 가져오는 기능 구현

---
+++ backend/src/main/java/org/dgu/backend/service/BackTestingServiceImpl.java
@@ -0,0 +1,36 @@
+package org.dgu.backend.service;
+
+import lombok.RequiredArgsConstructor;
+import org.dgu.backend.domain.Candle;
+import org.dgu.backend.domain.CandleInfo;
+import org.dgu.backend.dto.BackTestingDto;
+import org.dgu.backend.repository.CandleInfoRepository;
+import org.dgu.backend.repository.CandleRepository;
+import org.dgu.backend.util.BackTestingUtil;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+@RequiredArgsConstructor
+public class BackTestingServiceImpl implements BackTestingService {
+    private final BackTestingUtil backTestingUtil;
+    private final CandleInfoRepository candleInfoRepository;
+    private final CandleRepository candleRepository;
+
+    // 백테스팅 실행
+    @Override
+    public BackTestingDto.BackTestingResult runBackTesting(String authorizationHeader, BackTestingDto.StepInfo stepInfo) {
+        Candle candle = candleRepository.findByName(stepInfo.getCandleName());
+
+        // 캔들 ID에 해당하는 모든 CandleInfo 가져옴
+        List<CandleInfo> allCandleInfoList = candleInfoRepository.findAllByCandleId(candle.getId());
+
+        // 설정한 기간에 맞는 차트만 필터링
+        List<CandleInfo> filteredCandleInfoList = backTestingUtil.getFilteredCandleInfoList(allCandleInfoList, stepInfo.getStartDate(), stepInfo.getEndDate());
+
+        // 백테스팅 실행
+        //backTestingUtil.run(filteredCandleInfoList);
+        return null;
+    }
+}
