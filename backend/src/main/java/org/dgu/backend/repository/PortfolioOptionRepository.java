c94eb2b7f26d3aa0af484e2e3304d9025124235a
bbbang105 <2018111366@dgu.ac.kr>
2024-05-08 13:59:02 +0900

    #13 [rename] : 이름 변경

--- backend/src/main/java/org/dgu/backend/repository/PortfolioOptionRepository.java
+++ backend/src/main/java/org/dgu/backend/repository/PortfolioOptionRepository.java
@@ -1,9 +1,9 @@
 package org.dgu.backend.repository;
 
-import org.dgu.backend.domain.Portfolio;
+import org.dgu.backend.domain.PortfolioOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PortFolioRepository extends JpaRepository<Portfolio,Long> {
+public interface PortfolioOptionRepository extends JpaRepository<PortfolioOption,Long> {
 }
--- backend/src/main/java/org/dgu/backend/repository/PortfolioRepository.java
+++ backend/src/main/java/org/dgu/backend/repository/PortfolioRepository.java
@@ -1,9 +1,9 @@
 package org.dgu.backend.repository;
 
-import org.dgu.backend.domain.PortfolioOption;
+import org.dgu.backend.domain.Portfolio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PortFolioOptionRepository extends JpaRepository<PortfolioOption,Long> {
+public interface PortfolioRepository extends JpaRepository<Portfolio,Long> {
 }
