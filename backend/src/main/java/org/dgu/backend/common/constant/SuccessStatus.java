834de44111e4c271a60b324ab1f0dabfc424006a
bbbang105 <2018111366@dgu.ac.kr>
2024-05-17 01:47:01 +0900

    #21 [feat] : 포트폴리오 상세 조회 Enum 추가

--- backend/src/main/java/org/dgu/backend/common/constant/SuccessStatus.java
+++ backend/src/main/java/org/dgu/backend/common/constant/SuccessStatus.java
@@ -23,7 +23,8 @@ public enum SuccessStatus implements BaseCode {
     // User
     SUCCESS_LOGOUT(HttpStatus.OK, "200", "유저 로그아웃에 성공했습니다."),
     // Portfolio
-    SUCCESS_GET_PORTFOLIOS(HttpStatus.OK, "200", "포트폴리오 조회에 성공했습니다.");
+    SUCCESS_GET_PORTFOLIOS(HttpStatus.OK, "200", "포트폴리오 조회에 성공했습니다."),
+    SUCCESS_GET_PORTFOLIO_DETAILS(HttpStatus.OK, "200", "포트폴리오 상세 조회에 성공했습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
