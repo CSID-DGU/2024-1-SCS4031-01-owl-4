04b76a89d69137acb2d71e7b3c56856fbdb18724
bbbang105 <2018111366@dgu.ac.kr>
2024-06-03 23:36:36 +0900

    #21 [feat] : 가장 최근 포트폴리오를 가져오는 메서드 추가

--- backend/src/main/java/org/dgu/backend/repository/PortfolioRepository.java
+++ backend/src/main/java/org/dgu/backend/repository/PortfolioRepository.java
@@ -13,7 +13,11 @@ import java.util.UUID;
 @Repository
 public interface PortfolioRepository extends JpaRepository<Portfolio,Long> {
     Optional<Portfolio> findByPortfolioId(UUID portfolioId);
-    @Query("SELECT p FROM Portfolio p WHERE p.user = :user AND p.isSaved = 1")
+
+    @Query("SELECT p FROM Portfolio p WHERE p.user = :user AND p.isSaved = true")
     List<Portfolio> findAllSavedByUser(User user);
+
     Optional<Portfolio> findByUserAndPortfolioId(User user, UUID portfolioId);
+
+    Optional<Portfolio> findTopByUserOrderByCreatedAtDesc(User user);
 }
