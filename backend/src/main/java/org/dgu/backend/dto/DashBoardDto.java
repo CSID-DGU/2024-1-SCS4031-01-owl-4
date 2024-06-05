f0754278cc1c2ae38f827e48539cb29f51551a00
bbbang105 <2018111366@dgu.ac.kr>
2024-06-05 21:47:44 +0900

    #42 [feat] : 유저 보유 코인 응답 DTO 추가

--- backend/src/main/java/org/dgu/backend/dto/DashBoardDto.java
+++ backend/src/main/java/org/dgu/backend/dto/DashBoardDto.java
@@ -3,9 +3,15 @@ package org.dgu.backend.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import org.dgu.backend.domain.User;
+import org.dgu.backend.domain.UserCoin;
+import org.dgu.backend.util.BigDecimalSerializer;
+
+import java.math.BigDecimal;
 
 public class DashBoardDto {
     @Builder
@@ -15,5 +21,27 @@ public class DashBoardDto {
     @JsonInclude(JsonInclude.Include.NON_NULL)
     public static class UserAccountResponse {
         private Long account;
+    }
+
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class UserCoinResponse {
+        private String coinName;
+        @JsonSerialize(using = BigDecimalSerializer.class)
+        private BigDecimal balance;
+        @JsonSerialize(using = BigDecimalSerializer.class)
+        private BigDecimal price;
+        private Boolean isIncrease;
+
+        public UserCoin to(User user) {
+            return UserCoin.builder()
+                    .user(user)
+                    .coinName(coinName)
+                    .balance(balance)
+                    .build();
+        }
     }
 }
