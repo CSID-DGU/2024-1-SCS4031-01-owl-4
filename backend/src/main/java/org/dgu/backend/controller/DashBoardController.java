43e9e1adce4ff8b70535782447fd8a7ba195b82c
bbbang105 <2018111366@dgu.ac.kr>
2024-06-05 21:50:22 +0900

    #42 [feat] : 유저 업비트 보유 코인 조회 API 구현 (GET)

--- backend/src/main/java/org/dgu/backend/controller/DashBoardController.java
+++ backend/src/main/java/org/dgu/backend/controller/DashBoardController.java
@@ -8,6 +8,8 @@ import org.dgu.backend.service.DashBoardService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/dashboard")
 @RequiredArgsConstructor
@@ -21,5 +23,14 @@ public class DashBoardController {
 
         DashBoardDto.UserAccountResponse userBalanceResponse  = dashBoardService.getUserAccount(authorizationHeader);
         return ApiResponse.onSuccess(SuccessStatus.SUCCESS_GET_USER_BALANCE, userBalanceResponse);
+    }
+
+    // 유저 업비트 보유 코인 조회 API
+    @GetMapping("/coins/possession")
+    public ResponseEntity<ApiResponse<List<DashBoardDto.UserCoinResponse>>> getUserCoins(
+            @RequestHeader("Authorization") String authorizationHeader) {
+
+        List<DashBoardDto.UserCoinResponse> userCoins = dashBoardService.getUserCoins(authorizationHeader);
+        return ApiResponse.onSuccess(SuccessStatus.SUCCESS_GET_USER_COINS, userCoins);
     }
 }
