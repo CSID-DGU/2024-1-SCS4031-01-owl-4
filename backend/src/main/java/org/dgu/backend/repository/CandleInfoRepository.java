7f243b6ea896d3f3276e2d38d6171610737f764e
bbbang105 <2018111366@dgu.ac.kr>
2024-06-05 13:14:53 +0900

    #37 [refactor] : 불필요한 어노테이션 제거

--- backend/src/main/java/org/dgu/backend/repository/CandleInfoRepository.java
+++ backend/src/main/java/org/dgu/backend/repository/CandleInfoRepository.java
@@ -4,12 +4,10 @@ import org.dgu.backend.domain.Candle;
 import org.dgu.backend.domain.CandleInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Repository
 public interface CandleInfoRepository extends JpaRepository<CandleInfo,Long> {
     @Query("SELECT c FROM CandleInfo c WHERE c.candle = :candle AND c.dateTime BETWEEN :startDate AND :endDate ORDER BY c.dateTime")
     List<CandleInfo> findFilteredCandleInfo(Candle candle, LocalDateTime startDate, LocalDateTime endDate);
