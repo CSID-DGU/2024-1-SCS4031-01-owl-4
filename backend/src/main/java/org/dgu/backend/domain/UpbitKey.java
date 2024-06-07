0a2f957c3710880dee4382980a9e406014aae40e
bbbang105 <2018111366@dgu.ac.kr>
2024-06-07 17:08:24 +0900

    #48 [feat] : 프라이빗 키 컬럼 추가

--- backend/src/main/java/org/dgu/backend/domain/UpbitKey.java
+++ backend/src/main/java/org/dgu/backend/domain/UpbitKey.java
@@ -2,6 +2,7 @@ package org.dgu.backend.domain;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -22,11 +23,35 @@ public class UpbitKey {
     private User user;
 
     @Column(name = "upbit_keys_uuid", columnDefinition = "BINARY(16)", unique = true)
-    private UUID upbitKey;
+    private UUID upbitKeyId;
 
-    @Column(name = "access_key", nullable = false, length = 100)
+    @Column(name = "access_key", nullable = false, columnDefinition = "LONGTEXT")
     private String accessKey;
 
-    @Column(name = "secret_key", nullable = false, length = 100)
+    @Column(name = "secret_key", nullable = false, columnDefinition = "LONGTEXT")
     private String secretKey;
+
+    @Column(name = "private_key", nullable = false, columnDefinition = "LONGTEXT")
+    private String privateKey;
+
+    @Builder
+    public UpbitKey(User user, String accessKey, String secretKey, String privateKey) {
+        this.user = user;
+        this.upbitKeyId = UUID.randomUUID();
+        this.accessKey = accessKey;
+        this.secretKey = secretKey;
+        this.privateKey = privateKey;
+    }
+
+    public void updateAccessKey(String accessKey) {
+        this.accessKey = accessKey;
+    }
+
+    public void updateSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    public void updatePrivateKey(String privateKey) {
+        this.privateKey = privateKey;
+    }
 }
