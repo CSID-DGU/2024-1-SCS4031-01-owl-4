a267b979c1608c6da555ddcce6114f8c8648e444
bbbang105 <2018111366@dgu.ac.kr>
2024-06-09 03:27:55 +0900

    #53 [feat] : 코인 & 봉 & 기간에 따른 캔들 정보를 반환하는 기능 추가

--- backend/src/main/java/org/dgu/backend/repository/CandleInfoRepository.java
+++ backend/src/main/java/org/dgu/backend/repository/CandleInfoRepository.java
@@ -2,6 +2,7 @@ package org.dgu.backend.repository;
 
 import org.dgu.backend.domain.Candle;
 import org.dgu.backend.domain.CandleInfo;
+import org.dgu.backend.domain.Market;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -11,4 +12,7 @@ import java.util.List;
 public interface CandleInfoRepository extends JpaRepository<CandleInfo,Long> {
     @Query("SELECT c FROM CandleInfo c WHERE c.candle = :candle AND c.dateTime BETWEEN :startDate AND :endDate ORDER BY c.dateTime")
     List<CandleInfo> findFilteredCandleInfo(Candle candle, LocalDateTime startDate, LocalDateTime endDate);
+
+    @Query("SELECT c FROM CandleInfo c WHERE c.market = :market AND c.candle = :candle AND c.dateTime > :startDate ORDER BY c.dateTime")
+    List<CandleInfo> findByMarketAndCandleAndDateTimeAfter(Market market, Candle candle, LocalDateTime startDate);
 }
