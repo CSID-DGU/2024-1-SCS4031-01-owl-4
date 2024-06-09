84971d432a9e3afa5dbcd49d9caacec4b18e41eb
bbbang105 <2018111366@dgu.ac.kr>
2024-06-10 00:54:20 +0900

    #53 [style] : 변수명 변경

--- backend/src/main/java/org/dgu/backend/repository/CandleInfoRepository.java
+++ backend/src/main/java/org/dgu/backend/repository/CandleInfoRepository.java
@@ -10,9 +10,11 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface CandleInfoRepository extends JpaRepository<CandleInfo,Long> {
-    @Query("SELECT c FROM CandleInfo c WHERE c.candle = :candle AND c.dateTime BETWEEN :startDate AND :endDate ORDER BY c.dateTime")
-    List<CandleInfo> findFilteredCandleInfo(Candle candle, LocalDateTime startDate, LocalDateTime endDate);
+    @Query("SELECT c FROM CandleInfo c WHERE c.market = :market AND c.candle = :candle AND c.dateTime BETWEEN :startDate AND :endDate ORDER BY c.dateTime")
+    List<CandleInfo> findFilteredCandleInfo(Market market, Candle candle, LocalDateTime startDate, LocalDateTime endDate);
 
     @Query("SELECT c FROM CandleInfo c WHERE c.market = :market AND c.candle = :candle AND c.dateTime > :startDate ORDER BY c.dateTime")
     List<CandleInfo> findByMarketAndCandleAndDateTimeAfter(Market market, Candle candle, LocalDateTime startDate);
+
+    CandleInfo findTopByMarketAndCandleOrderByTimestampDesc(Market market, Candle candle);
 }
