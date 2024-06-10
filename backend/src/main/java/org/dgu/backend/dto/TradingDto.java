3564f02b30bb0ad3a1d7a009b58c410e9173fff8
bbbang105 <2018111366@dgu.ac.kr>
2024-06-10 19:30:30 +0900

    #33 [feat] : 자동매매 등록 DTO 추가

---
+++ backend/src/main/java/org/dgu/backend/dto/TradingDto.java
@@ -0,0 +1,42 @@
+package org.dgu.backend.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.dgu.backend.domain.Portfolio;
+import org.dgu.backend.domain.PortfolioOption;
+import org.dgu.backend.domain.TradingOption;
+import org.dgu.backend.domain.User;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class TradingDto {
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class AutoTradingRequest {
+        private UUID portfolioId;
+        private Long fund;
+        private LocalDateTime startDate;
+        private LocalDateTime endDate;
+
+        public TradingOption to(User user, Portfolio portfolio, PortfolioOption portfolioOption) {
+            return TradingOption.builder()
+                    .user(user)
+                    .portfolio(portfolio)
+                    .startCapital(BigDecimal.valueOf(fund))
+                    .tradingUnitPrice(BigDecimal.valueOf(fund / portfolioOption.getTradingUnit()))
+                    .remainedBuyingCount(portfolioOption.getTradingUnit())
+                    .startDate(startDate)
+                    .endDate(endDate)
+                    .build();
+        }
+    }
+}
