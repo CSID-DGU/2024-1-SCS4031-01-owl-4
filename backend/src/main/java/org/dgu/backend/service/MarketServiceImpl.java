e591982e2aeed56ffe29d3e6f938aed0e7638863
bbbang105 <2018111366@dgu.ac.kr>
2024-06-16 01:53:09 +0900

    #33 [refactor] : 메서드 분리

--- backend/src/main/java/org/dgu/backend/service/MarketServiceImpl.java
+++ backend/src/main/java/org/dgu/backend/service/MarketServiceImpl.java
@@ -17,32 +17,18 @@ import org.springframework.web.client.RestTemplate;
 public class MarketServiceImpl implements MarketService {
     @Value("${upbit.url.market}")
     private String UPBIT_URL_MARKET;
-    private final RestTemplate restTemplate;
+    private final UpbitApiClient upbitApiClient;
     private final MarketRepository marketRepository;
 
     // 모든 가상화폐 데이터를 가져와 저장하는 메서드
     @Override
     public void getAllMarkets() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("accept", MediaType.APPLICATION_JSON_VALUE);
-
-        ResponseEntity<UpbitDto.MarketResponse[]> responseEntity = restTemplate.exchange(
-                UPBIT_URL_MARKET,
-                HttpMethod.GET,
-                new HttpEntity<>(headers),
-                UpbitDto.MarketResponse[].class
-        );
-
-        UpbitDto.MarketResponse[] responseBody = responseEntity.getBody();
-        if (responseBody != null) {
-            for (UpbitDto.MarketResponse marketResponse : responseBody) {
-                // "KRW-"로 시작하는 가상화폐만 저장
-                if (marketResponse.getName().startsWith("KRW-")) {
-                    marketRepository.save(marketResponse.to());
-                }
+        UpbitDto.MarketResponse[] responseBody = upbitApiClient.getAllMarketsAtUpbit(UPBIT_URL_MARKET);
+        for (UpbitDto.MarketResponse marketResponse : responseBody) {
+            // "KRW-"로 시작하는 가상화폐만 저장
+            if (marketResponse.getName().startsWith("KRW-")) {
+                marketRepository.save(marketResponse.to());
             }
-        } else {
-            log.error("Failed to receive market info");
         }
     }
 }
