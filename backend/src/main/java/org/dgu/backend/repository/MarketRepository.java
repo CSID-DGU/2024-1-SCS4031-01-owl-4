9b378ded8ef5da08a0fb590dee329b2abffd08c3
bbbang105 <2018111366@dgu.ac.kr>
2024-06-15 13:24:56 +0900

    #42 [feat] : 마켓 이름으로 찾는 기능 추가

--- backend/src/main/java/org/dgu/backend/repository/MarketRepository.java
+++ backend/src/main/java/org/dgu/backend/repository/MarketRepository.java
@@ -4,5 +4,6 @@ import org.dgu.backend.domain.Market;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MarketRepository extends JpaRepository<Market,Long> {
-    Market findByKoreanName(String marketKoreanName);
+    Market findByKoreanName(String koreanName);
+    Market findByMarketName(String marketName);
 }
