dd624227bb6135e35265aea32b31c6ea89d3248f
Naeun <104514884+nanaono@users.noreply.github.com>
2024-06-18 22:41:07 +0900

    Update test.py

--- AI/coin_priceprediction/test.py
+++ AI/coin_priceprediction/test.py
@@ -8,9 +8,9 @@ import os
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
-# API 키 설정 (주의: 실제 코드에서는 보안상 API 키를 노출시키지 않도록 주의해야 합니다)
-access_key = "3rCnY46Xw4Mx9q2tVrWGpzUPBtAjim82ZMn508Bd"
-secret_key = "R1t5gFoPNZNTXaBlWHbdx85hPPvdhvSSZx2Y9uA7"
+# API 키 설정 (코드 사용하려면 추가하기)
+access_key = ""
+secret_key = ""
 
 # 데이터 수집 기간 설정
 start_date = datetime.datetime(2018, 1, 1)
