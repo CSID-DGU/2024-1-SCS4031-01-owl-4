4e09ff5f81950111722726d076d54c30f6a7d17c
bbbang105 <2018111366@dgu.ac.kr>
2024-06-19 02:22:48 +0900

    #68 [feat] : 딥러닝 가격예측값 DTO 추가

---
+++ backend/src/main/java/org/dgu/backend/dto/PredictionDto.java
@@ -0,0 +1,23 @@
+package org.dgu.backend.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+public class PredictionDto {
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class PredictionResponse {
+        @JsonProperty("date")
+        private String date;
+        @JsonProperty("close")
+        private Long close;
+    }
+}
